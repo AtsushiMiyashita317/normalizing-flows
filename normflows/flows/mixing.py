7f10ea0a1a46b9c6c4fc064c17ade45715fd1582
AtsushiMiyashita317 <81409182+AtsushiMiyashita317@users.noreply.github.com>
2025-12-25 00:59:00 +0900

    Replace triangular_solve with linalg.solve_triangular

--- normflows/flows/mixing.py
+++ normflows/flows/mixing.py
@@ -93,11 +93,11 @@ class Invertible1x1Conv(Flow):
         )
         if inverse:
             if self.log_S.dtype == torch.float64:
-                L_inv = torch.triangular_solve(self.eye, L, upper=False, unitriangular=True)
-                U_inv = torch.triangular_solve(self.eye, U, upper=True, unitriangular=False)
+                L_inv = torch.linalg.solve_triangular(L, self.eye, upper=False, unitriangular=True)
+                U_inv = torch.linalg.solve_triangular(U, self.eye, upper=True, unitriangular=False)
             else:
-                L_inv = torch.triangular_solve(self.eye.double(), L.double(), upper=False, unitriangular=True).solution.type(self.log_S.dtype)
-                U_inv = torch.triangular_solve(self.eye.double(), U.double(), upper=True, unitriangular=False).solution.type(self.log_S.dtype)
+                L_inv = torch.linalg.solve_triangular(L.double(), self.eye.double(), upper=False, unitriangular=True).type(self.log_S.dtype)
+                U_inv = torch.linalg.solve_triangular(U.double(), self.eye.double(), upper=True, unitriangular=False).type(self.log_S.dtype)
             W = U_inv @ L_inv @ self.P.t()
         else:
             W = self.P @ L @ U
